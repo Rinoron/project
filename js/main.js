59dc839262a4e676951b6a3844d9cfce290aae02
Musafir <example@gmail.com>
2022-06-28 23:27:28 +0300

    log

---
+++ js/main.js
@@ -0,0 +1,194 @@
+// console.log(33);
+
+// "use strict";
+
+// a = 2244
+// console.log(a);
+
+// let number = 33;
+
+// number = 22;
+
+// const leftBorderWidth = 7;
+
+// console.log(leftBorderWidth);
+
+// const obj = {
+//   a: 22,
+// };
+
+// obj.a = 33;
+// obj.b = "keito";
+
+// obj.age = 34;
+
+// delete obj.b;
+
+// console.log(obj);
+
+// {
+//   var result = 22;
+// }
+
+// console.log(result);
+
+// const carBodyWidth = 5000;
+
+// const carBodyLength = 4000;
+
+// console.log(`Ширина кузова авто:${carBodyWidth}, длина:${carBodyLength}`);
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+// camelCase
+
+// let num = 12321;
+
+// console.log(num);
+
+// console.log(-8 / 0);
+
+// console.log("num" * 9);
+
+// const persone = "Adam 334";
+
+// console.log(persone);
+
+// let und;
+
+// console.log(und);
+
+// const obj = {
+//   name: "Adam",
+//   age: 33,
+//   isMarried: false,
+// };
+
+// console.log(obj.age);
+
+// let arr = ["adam", "red", 33, {}, [2]];
+
+// console.log(arr[3]);
+
+// const arr = [1, 2, 3];
+
+// console.log(arr[1]);
+
+// const obj = { a: 1, b: 2 };
+
+// const obj = {
+//   anna: 500,
+//   alice: 800,
+// };
+
+// console.log(obj);
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// const arr = [1, 2, 3];
+
+// arr[10] = 3445;
+
+// console.log(arr);
+
+// const arrObj = {
+//   1: "a",
+//   2: "b",
+//   3: "c",
+//   abc: {
+//     id: [{ a: 3 }, { b: 4 }],
+//     a: 1,
+//     b: 2,
+//     c: 3,
+//   },
+// };
+
+// arrObj.mm = [33];
+
+// console.log(arrObj);
+
+// const m = "m";
+
+// arrObj[4] = 33;
+// arrObj.m = 33;
+
+// console.log(arrObj[4]);
+// console.log(arrObj.m);
+
+// const storeName = "Fawaid";
+// const storeDescription = {
+//   budget: 10000,
+//   employees: ["Umar", "Adam", "Ysuf"],
+//   products: {
+//     ajwa: 1000,
+//     caraway: 500,
+//   },
+//   open: true,
+// };
+//  console.log(storeDescription);
+
+// alert("salam");
+
+// const result = confirm("are you here?");
+
+// console.log(result);
+
+// const answer = prompt("Вам есть 33?"); // тип всегда string
+
+// const answer = +prompt("Вам есть 33?"); // + меняет тип на number
+
+// console.log(answer + 33);
+
+// const answers = [];
+
+// answers[0] = prompt("wat you name?");
+// answers[1] = prompt("wat you surName?");
+// answers[2] = prompt("wat you age?");
+
+// console.log(typeof answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/7`);
+
+// const user = 'Adam';
+
+// alert(`hello ${user}`);
+
+// console.log('arr' + ' - obj');
+
+// console.log(4 + +'6');
+
+// console.log(4 + '6');
+
+
+// let incr = 10,
+
+// 	 decr = 10;
+
+	//  incr++;
+	//  ++incr;
+
+	//  decr--;
+	//  --decr;
+
+	//  console.log(++incr);
+	//  console.log(--decr);
+
+// console.log(5 >= 2);
+
+// console.log(2*4 === 8);
+
+// && // и
+
+// || // или
+
+// const isChecked = false, isClose = false;
+
+// console.log(isChecked || !isClose);
+
+// console.log(2 + 2 * 2 == 6);
+
+console.log('git');
+
